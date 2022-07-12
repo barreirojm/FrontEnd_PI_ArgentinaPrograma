ee61607b176fc1c01fdfcd5448b6a52407c0010d
barreirojm <barreirojuanmanuel@gmail.com>
2022-07-12 17:46:30 -0300

    experiencia

--- PI_barreirojm/src/app/app.module.ts
+++ PI_barreirojm/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoApComponent,
     RedesSocialesComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
